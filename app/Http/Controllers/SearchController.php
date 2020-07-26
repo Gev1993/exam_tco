27d9c1f21e24ff75b961f1b3436897781694f352
Gev1993 <61912309+Gev1993@users.noreply.github.com>
2020-07-26 21:11:59 +0400

    Create SearchController.php

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\management;
+use App\Model\Product;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function getResults(Request $request)
+    {
+        $search = $request->input('search');
+        if (!$search) {
+            redirect()->route('managements.index');
+        }
+
+        $search = management::orWhere("name", "LIKE", "%{$search}%")->get();
+
+
+        return view('search.results')->with('search', $search);
+    }
+
+    public function setResults(Request $request)
+    {
+        $search = $request->input('search');
+        if (!$search) {
+            redirect()->route('products.index');
+        }
+
+        $search = Product::orWhere("name", "LIKE", "%{$search}%")->get();
+
+
+        return view('search.results1')->with('search', $search);
+    }
+
+}
+
+
